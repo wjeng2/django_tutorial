dd4594cf050929917b17041ea044feda0cd92535
Wei Wen Jeng <wjeng@andrew.cmu.edu>
2020-06-03 10:19:58 -0400

    finished tutorial 5 relationship and hyperlink APIs

--- rest-tutorial/tutorial/snippets/serializers.py
+++ rest-tutorial/tutorial/snippets/serializers.py
@@ -1,20 +1,38 @@
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
-#tutorial 4
-from django.contrib.auth.models import User
+# #tutorial 4
+# from django.contrib.auth.models import User
 
-class SnippetSerializer(serializers.ModelSerializer):
-	#tutorail 4
-	owner = serializers.ReadOnlyField(source='owner.username')
+# class SnippetSerializer(serializers.ModelSerializer):
+# 	#tutorail 4
+# 	owner = serializers.ReadOnlyField(source='owner.username')
 
-	class Meta:
-		model = Snippet
-		fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
+# 	class Meta:
+# 		model = Snippet
+# 		fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
 
-class UserSerializer(serializers.ModelSerializer):
-	snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+# class UserSerializer(serializers.ModelSerializer):
+# 	snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
 
-	class Meta:
-		model = User
-		fields = ['id', 'username', 'snippets']
+# 	class Meta:
+# 		model = User
+# 		fields = ['id', 'username', 'snippets']
+
+#tutorail 5 addition
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
+    class Meta:
+        model = Snippet
+        fields = ['url', 'id', 'owner',
+                  'title', 'code', 'highlight','linenos', 'language', 'style']
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['url', 'id', 'username', 'snippets']
--- rest-tutorial/tutorial/snippets/views.py
+++ rest-tutorial/tutorial/snippets/views.py
@@ -34,6 +34,13 @@ from snippets.serializers import UserSerializer
 from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
 
+#tutorial 5
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
+
+
 
 """
 -----------------------------------
@@ -215,3 +222,24 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
+
+"""
+------------------------------
+#tutorial 5
+------------------------------
+"""
+@api_view(['GET'])
+def api_root(request, format=None):
+	#reverse = return fully-qualified URL?
+	return Response({
+		'users': reverse('user-list', request=request, format=format),
+		'snippets': reverse('snippet-list', request=request, format=format)
+		})
+
+class SnippetHighlight(generics.GenericAPIView):
+	queryset = Snippet.objects.all()
+	renderer_classes = [renderers.StaticHTMLRenderer]
+
+	def get(self, request, *args, **kwargs):
+		snippet = self.get_object()
+		return Response(snippet.highlighted)
