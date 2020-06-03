367da5ac6f276f08383102492d89d2fc2329e07c
Wei Wen Jeng <wjeng@andrew.cmu.edu>
2020-06-03 10:40:03 -0400

    small change to tutorial 5 files

--- rest-tutorial/tutorial/snippets/serializers.py
+++ rest-tutorial/tutorial/snippets/serializers.py
@@ -1,23 +1,23 @@
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
-# #tutorial 4
-# from django.contrib.auth.models import User
+#tutorial 4
+from django.contrib.auth.models import User
 
-# class SnippetSerializer(serializers.ModelSerializer):
-# 	#tutorail 4
-# 	owner = serializers.ReadOnlyField(source='owner.username')
+class SnippetSerializer(serializers.ModelSerializer):
+	#tutorail 4
+	owner = serializers.ReadOnlyField(source='owner.username')
 
-# 	class Meta:
-# 		model = Snippet
-# 		fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
+	class Meta:
+		model = Snippet
+		fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
 
-# class UserSerializer(serializers.ModelSerializer):
-# 	snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+class UserSerializer(serializers.ModelSerializer):
+	snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
 
-# 	class Meta:
-# 		model = User
-# 		fields = ['id', 'username', 'snippets']
+	class Meta:
+		model = User
+		fields = ['id', 'username', 'snippets']
 
 #tutorail 5 addition
 class SnippetSerializer(serializers.HyperlinkedModelSerializer):
